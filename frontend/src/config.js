aad2b5bf0f7634d14ad6e0db2e0ffcc67bf7016d
Milan <milanmatejicdev@gmail.comn>
2022-05-02 21:03:58 +0200

    Building our React app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,19 +1,22 @@
 const config = {
-    // Backend config
-    s3: {
-      REGION: process.env.REACT_APP_REGION,
-      BUCKET: process.env.REACT_APP_BUCKET,
-    },
-    apiGateway: {
-      REGION: process.env.REACT_APP_REGION,
-      URL: process.env.REACT_APP_API_URL,
-    },
-    cognito: {
-      REGION: process.env.REACT_APP_REGION,
-      USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
-      APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
-      IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
-    },
-  };
-  
-  export default config;
+  STRIPE_KEY:
+    "pk_test_51KtzzBG1fchmYzLhPftn8ubchW1mTcq17g81oAPAZzETEzldzVJSGOxfBMujR8Qtzct2zgu5wbhym1mhyaP1p5Dp00E4NYayDW",
+  MAX_ATTACHMENT_SIZE: 5000000,
+  // Backend config
+  s3: {
+    REGION: process.env.REACT_APP_REGION,
+    BUCKET: process.env.REACT_APP_BUCKET,
+  },
+  apiGateway: {
+    REGION: process.env.REACT_APP_REGION,
+    URL: process.env.REACT_APP_API_URL,
+  },
+  cognito: {
+    REGION: process.env.REACT_APP_REGION,
+    USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+    APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
+  },
+};
+
+export default config;
